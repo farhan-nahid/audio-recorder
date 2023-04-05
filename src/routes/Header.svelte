864f0a1fceb20b753aece50f217101de5f03d4ab
Farhan Ahmed Nahid <nahidahmed9933@gmail.com>
2023-04-05 10:41:04 +0600

    recorder added

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
+	import logo from '$lib/images/svelte-logo.svg';
 </script>
 
 <header>
@@ -24,6 +24,9 @@
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/recorder' ? 'page' : undefined}>
+				<a href="/recorder">Recorder</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
